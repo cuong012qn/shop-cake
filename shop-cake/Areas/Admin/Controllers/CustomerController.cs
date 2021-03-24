8f21ffcff3d72ab575bc10973e5e595495c7b6ce
cuong012qn <cuong012qn121@gmail.com>
2021-03-24 12:40:25 +0700

    remove Migrations

---
+++ shop-cake/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using shop_cake.Data;
+using Microsoft.EntityFrameworkCore;
+using shop_cake.Extensions;
+using shop_cake.Models;
+
+namespace shop_cake.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomerController : Controller
+    {
+        private readonly ShopCakeDBContext _context;
+
+        public CustomerController(ShopCakeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CustomerController
+        public async Task<ActionResult> Index(int? pageNumber)
+        {
+            return View(await PaginatedList<Customer>.CreateAsync(_context.Customers, pageNumber ?? 1, 10));
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            try
+            {
+                await _context.Customers.AddAsync(customer);
+                await _context.SaveChangesAsync();
+                return View(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            return View(customer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
